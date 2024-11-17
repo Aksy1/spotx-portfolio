6d0ddf7d8a053e2420d80469335266dc1889985c
Aksy1 <140134270+Aksy1@users.noreply.github.com>
2024-11-18 00:13:55 +0530

    Update settings.py

--- mainportfolio/portfolio/settings.py
+++ mainportfolio/portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#w6h_yn49@dbuvygcd-)fd6vmapejt$jo&)57yk+ueyg&)tql(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://spotx-portfolio.onrender.com"]
 
 
 # Application definition
